682b4472ecb8a3777de4a6579c05c3eab04d3713
kAleks12 <kacper4553@gmail.com>
2023-09-26 09:51:09 +0200

    Add better submission handling

--- src/components/home/SubmissionCard.js
+++ src/components/home/SubmissionCard.js
@@ -1,27 +1,33 @@
 import * as React from "react";
-import {FaRegCalendarAlt, FaTags, FaMapMarkerAlt, FaUser} from 'react-icons/fa';
+import {FaRegCalendarAlt, FaTags, FaMapMarkerAlt, FaUser, FaEnvelope} from 'react-icons/fa';
 import "./SubmissionCard.css"
 
 const SubmissionCard = ({image, form}) => {
+    if (form?.tags.length === 0) {
+        form?.tags.push({value: "No tags provided"});
+    }
+    const description = form?.description || "No description provided";
+
     return (
         <div className='submission-container'>
             <img src={image} alt="Submitted animal" style={{width: '100%', height: '200px', objectFit: 'cover'}}/>
             <p><FaRegCalendarAlt/> {form?.date}</p>
             <p><FaMapMarkerAlt/> lon = {form?.x} ; lan = {form?.y}</p>
-            <p><FaUser/> {form?.user} </p>
+            <p><FaUser/> {form?.author?.username}</p>
+            <p><FaEnvelope/> {form?.author?.email} </p>
             <div>
                 <FaTags/>
                 {form?.tags.map((tag, index) => (
                     <span key={index} style={{
                         display: 'inline-block',
                         margin: '0.5rem',
-                        padding: '5px',
+                        padding: '10px',
                         background: '#1c1c1c',
                         borderRadius: '5px'
                     }}>{tag.value}</span>
                 ))}
             </div>
-            <h2>{form?.description}</h2>
+            <h2>{description}</h2>
         </div>
     );
 };
